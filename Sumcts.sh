730480dea96a4744b423d4c6147fb6258d4c7154
Loriano Storchi <loriano@storchi.org>
2019-12-04 14:18:09 +0100

    fix

--- Sumcts.sh
+++ Sumcts.sh
@@ -1,12 +1,13 @@
 python2.7 pyct.py -f /pub3/redo/redo.bertha/Au20OgRotTranFine.nocv/fit-rho.cube /pub3/redo/redo.bertha/Au20OgRotTranFine.nocv/fit-rhoa.cube /pub3/redo/redo.bertha/Au20OgRotTranFine.nocv/fit-rhob.cube
 isodat=`awk 'FNR==1 {print $4}' isofile`
 
-Number = 26399
-while [[ $Number -gt 26335 ]]
-      printf "$Number\n"
-      ((Number -= 2 )
+export i=26399
+while [[ $i -gt 26335 ]]
+do
+      printf "$i\n"
 
-      fname=fit-pair.$Number
+      fname=fit-pair.$i
       python2.7 pycd.py -f /pub3/redo/redo.bertha/Au20OgRotTranFine.nocv/$fname.cube -i $isodat -o /pub3/redo/redo.bertha/Au20OgRotTranFine.nocv/$fname.dsv
-done
 
+      ((i -= 2 ))
+done
